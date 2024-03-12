e81e06549540da3472d0f2175866ad87d71da4ba
Yashv402 <112461733+Yashv402@users.noreply.github.com>
2024-03-12 23:04:34 +0530

    added login page and started navbar

---
+++ src/app/(routes)/jobs/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Leftnavbar from '@/components/Leftnavbar'
+
+
+const page = () => {
+  return (
+    <div><Leftnavbar/></div>
+  )
+}
+
+export default page
---
+++ src/components/Leftnavbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Leftnavbar = () => {
+  return (
+    <div className='bg-purple-300 w-[20%] h-[100vh]'>
+        <div>
+        <h2>Hii! i am Leftnavbar</h2>
+        <ul>
+            <li>Jobs</li>
+            <li>Companies</li>
+            <li>Skills</li>
+            <li>For you</li>
+        </ul>
+        </div>
+        <button>Profile :D </button>
+    </div>
+  )
+}
+
+export default Leftnavbar
