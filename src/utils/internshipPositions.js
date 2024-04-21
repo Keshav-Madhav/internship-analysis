4f522df7e95099dcf691f9e8b2404beeef31973a
Keshav Madhav <keshav2552003@gmail.com>
2024-04-21 23:55:34 +0530

    added temporary data

---
+++ src/utils/internshipPositions.js
@@ -0,0 +1,182 @@
+export const internshipsData = [
+  { 
+    position: 'Front End Dev', 
+    count: 30,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["HTML","CSS","JavaScript","React","Angular","Responsive Design"]
+  },
+  { 
+    position: 'Backend Dev', 
+    count: 35,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Node.js","Express.js","Django","Flask","SQL","RESTful APIs"]
+  },
+  { 
+    position: 'Graphic Designer', 
+    count: 40,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Adobe Photoshop","Sketch","Figma","Typography","Color Theory"]
+  },
+  { 
+    position: 'Content Writer', 
+    count: 25,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Copywriting","SEO","Grammar","Research","Storytelling"]
+  },
+  { 
+    position: 'Data Analyst', 
+    count: 20,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["SQL","Python","Data Visualization","Machine Learning","Data Cleaning"]
+  },
+  { 
+    position: 'UI/UX Designer', 
+    count: 30,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["UI Design","UX Design","Adobe XD","Sketch","Figma"]
+  },
+  { 
+    position: 'Mobile App Dev', 
+    count: 35,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Java","React Native","Flutter","iOS","Android","Mobile UI Design"]
+  },
+  { 
+    position: 'SEO Specialist', 
+    count: 15,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Keyword Research","On-Page SEO","Off-Page SEO","Google Analytics","SEO Tools"]
+  },
+  { 
+    position: 'Digital Marketer', 
+    count: 20,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Social Media Marketing","SEO","Email Marketing","Content Marketing","Google Ads"]
+  },
+  { 
+    position: 'Network Engineer', 
+    count: 10,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Cisco","LAN/WAN","TCP/IP","Routing","Network Security","Troubleshooting"]
+  },
+  { 
+    position: 'Database Admin', 
+    count: 15,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["SQL","Database Management","Database Design","Backup and Recovery","Data Security"]
+  },
+  { 
+    position: 'System Analyst', 
+    count: 10,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["System Analysis","Requirement Gathering","UML","System Design","Agile"]
+  },
+  { 
+    position: 'Information Security', 
+    count: 5,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Cybersecurity","Network Security","Penetration Testing","Security Policies","Encryption"]
+  },
+  { 
+    position: 'Full Stack Dev', 
+    count: 30,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["HTML","CSS","JavaScript","Node.js","Express.js","React","Angular","MongoDB","RESTful APIs"]
+  },
+  { 
+    position: 'Product Manager', 
+    count: 10,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Product Management","Agile Methodologies","Scrum","User Stories","Market Research"]
+  },
+  { 
+    position: 'Project Manager', 
+    count: 12,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Project Management","Leadership","Risk Management","Stakeholder Management","Agile"]
+  },
+  { 
+    position: 'Software Engineer', 
+    count: 25,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Software Development","Algorithms","Data Structures","Object-Oriented Programming","Problem Solving"]
+  },
+  { 
+    position: 'DevOps Engineer', 
+    count: 20,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["DevOps","CI/CD","Infrastructure as Code","Containerization","Cloud Computing"]
+  },
+  { 
+    position: 'Machine Learning Engineer', 
+    count: 15,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Machine Learning","Deep Learning","Data Science","Python","TensorFlow"]
+  },
+  { 
+    position: 'AI Specialist', 
+    count: 10,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Artificial Intelligence","Machine Learning","Deep Learning","Natural Language Processing","Computer Vision"]
+  },
+  { 
+    position: 'Cloud Architect', 
+    count: 8,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Cloud Computing","Amazon Web Services (AWS)","Microsoft Azure","Google Cloud Platform (GCP)","Architecture Design"]
+  },
+  { 
+    position: 'Blockchain Developer', 
+    count: 5,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Blockchain","Smart Contracts","Cryptocurrency","Ethereum","Hyperledger"]
+  },
+  { 
+    position: 'Game Developer', 
+    count: 15,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Game Development","Unity","C#","3D Modeling","Game Design"]
+  },
+  { 
+    position: 'AR/VR Developer', 
+    count: 10,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Augmented Reality (AR)","Virtual Reality (VR)","Unity","C#","3D Modeling"]
+  },
+  { 
+    position: 'Big Data Engineer', 
+    count: 12,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Big Data","Hadoop","Spark","Scala","Data Engineering"]
+  },
+  { 
+    position: 'Embedded Systems Engineer', 
+    count: 8,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Embedded Systems","Microcontrollers","C/C++","RTOS","Hardware Design"]
+  },
+  { 
+    position: 'Robotics Engineer', 
+    count: 6,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Robotics","ROS","Computer Vision","Python","Mechanical Engineering"]
+  },
+  { 
+    position: 'UI Developer', 
+    count: 20,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["HTML","CSS","JavaScript","React","Angular","Responsive Design"]
+  },
+  { 
+    position: 'UX Developer', 
+    count: 18,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["UX Design","User Research","Wireframing","Prototyping","Usability Testing"]
+  },
+  { 
+    position: 'Software Tester', 
+    count: 10,
+    img: 'https://images.pexels.com/photos/7688460/pexels-photo-7688460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    skills: ["Software Testing","Manual Testing","Automated Testing","Test Planning","Bug Tracking"]
+  },
+];
